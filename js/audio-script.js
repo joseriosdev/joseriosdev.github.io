759158bf412acbddadca189e200bd83410a0a4d0
Jose Rios <joseriosdev@gmail.com>
2021-04-08 21:40:59 -0500

    tolkien project added

--- js/audio-script.js
+++ js/audio-script.js
@@ -2,8 +2,4 @@ let audio = document.getElementById("audio");
 let projectItem = document.querySelectorAll(".project-item");
 audio.volume = .7;
 
-projectItem.forEach(element => element.addEventListener("mouseover", playSfx));
-
-function playSfx() {
-  audio.play();
-}
+projectItem.forEach(element => element.addEventListener("mouseover", () => audio.play()));
